13bf6959969f388fc16b8d5de3bfbbba574f1bea
Lane Aasen <laneaasen@gmail.com>
2013-11-10 03:30:20 -0800

    topics are now plotted on a circle, with radius correlated to number of references

--- visual/visual.go
+++ visual/visual.go
@@ -2,6 +2,7 @@ package visual
 
 import (
 	"io"
+	"math"
 	"math/rand"
 
 	"github.com/aaasen/kapok/graph"
@@ -13,23 +14,28 @@ const height = 1024
 
 type positions struct {
 	Positions map[string]*Vector
+	Graph     *graph.Graph
 }
 
-func newPositions() *positions {
+func newPositions(graph *graph.Graph) *positions {
 	return &positions{
 		Positions: make(map[string]*Vector),
+		Graph:     graph,
 	}
 }
 
-func (self *positions) SafeGet(name string) *Vector {
-	vector := self.Positions[name]
+func (self *positions) SafeGet(node *graph.Node) *Vector {
+	vector := self.Positions[node.Name]
 
 	if vector == nil {
+		angle := rand.Float64() * math.Pi * 2
+		r := (width / 2.5) / float64(len(self.Graph.Nodes[node])+1)
+
 		vector = &Vector{
-			X: rand.Intn(width),
-			Y: rand.Intn(height),
+			X: int((width / 2) + math.Cos(angle)*r),
+			Y: int((height / 2) + math.Sin(angle)*r),
 		}
-		self.Positions[name] = vector
+		self.Positions[node.Name] = vector
 	}
 
 	return vector
@@ -39,10 +45,10 @@ func Visualise(g *graph.Graph, writer io.Writer) *svg.SVG {
 	canvas := svg.New(writer)
 	canvas.Start(width, height)
 
-	positionsA := newPositions()
+	positionsA := newPositions(g)
 
 	for node, _ := range g.Nodes {
-		nodePos := positionsA.SafeGet(node.Name)
+		nodePos := positionsA.SafeGet(node)
 
 		canvas.Circle(
 			nodePos.X,
@@ -51,13 +57,20 @@ func Visualise(g *graph.Graph, writer io.Writer) *svg.SVG {
 			"fill:black")
 
 		for neighbor, _ := range g.Nodes[node] {
-			neighborPos := positionsA.SafeGet(neighbor.Name)
+			neighborPos := positionsA.SafeGet(neighbor)
 
 			canvas.Line(
 				nodePos.X, nodePos.Y,
 				neighborPos.X, neighborPos.Y,
 				"stroke:rgba(0, 0, 0, 0.2);stroke-width:0.5")
 		}
+
+		canvas.Text(
+			nodePos.X,
+			nodePos.Y,
+			node.Name,
+			"text-anchor:middle;font-size:12px;fill:#5bb4c0;")
+
 	}
 
 	canvas.End()
