8c4d0523e312fb92deb8bc2127f9a5af9d7374c5
Lane Aasen <laneaasen@gmail.com>
2013-11-10 02:44:09 -0800

    simple visualisation of small graphs as svgs working!

---
+++ visual/vector.go
@@ -0,0 +1,5 @@
+package visual
+
+type Vector struct {
+	X, Y int
+}
--- visual/visual.go
+++ visual/visual.go
@@ -8,14 +8,56 @@ import (
 	svg "github.com/ajstarks/svgo"
 )
 
-func Visualise(graph *graph.Graph, writer io.Writer) *svg.SVG {
-	width := 1024
-	height := 1024
+const width = 1024
+const height = 1024
+
+type positions struct {
+	Positions map[string]*Vector
+}
+
+func newPositions() *positions {
+	return &positions{
+		Positions: make(map[string]*Vector),
+	}
+}
+
+func (self *positions) SafeGet(name string) *Vector {
+	vector := self.Positions[name]
+
+	if vector == nil {
+		vector = &Vector{
+			X: rand.Intn(width),
+			Y: rand.Intn(height),
+		}
+		self.Positions[name] = vector
+	}
+
+	return vector
+}
+
+func Visualise(g *graph.Graph, writer io.Writer) *svg.SVG {
 	canvas := svg.New(writer)
 	canvas.Start(width, height)
 
-	for k, _ := range graph.Nodes {
-		canvas.Text(int(rand.Float32()*float32(width)), int(rand.Float32()*float32(height)), k.Name, "text-anchor:middle;font-size:12px;fill:black")
+	positionsA := newPositions()
+
+	for node, _ := range g.Nodes {
+		nodePos := positionsA.SafeGet(node.Name)
+
+		canvas.Circle(
+			nodePos.X,
+			nodePos.Y,
+			1,
+			"fill:black")
+
+		for neighbor, _ := range g.Nodes[node] {
+			neighborPos := positionsA.SafeGet(neighbor.Name)
+
+			canvas.Line(
+				nodePos.X, nodePos.Y,
+				neighborPos.X, neighborPos.Y,
+				"stroke:rgba(0, 0, 0, 0.2);stroke-width:0.5")
+		}
 	}
 
 	canvas.End()
