15fc41c74243a6fc65a66fa2b888f2f904c61042
Lane Aasen <laneaasen@gmail.com>
2013-11-09 16:47:17 -0800

    adds unit tests for addArc, removeArc, and benchmarks

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -4,6 +4,25 @@ import (
 	"testing"
 )
 
+func getTestGraph() *Graph {
+	g := NewGraph()
+
+	a := NewNode("A")
+	b := NewNode("B")
+	c := NewNode("C")
+
+	g.Add(a)
+	g.Add(b)
+	g.Add(c)
+
+	g.AddArc(a, b)
+	g.AddArc(a, c)
+	g.AddArc(b, c)
+	g.AddArc(c, b)
+
+	return g
+}
+
 func TestCreate(t *testing.T) {
 	getTestGraph()
 }
@@ -34,12 +53,68 @@ func TestRemove(t *testing.T) {
 	}
 }
 
+func TestAddArc(t *testing.T) {
+	g := getTestGraph()
+	g.AddArc(g.Get("B"), g.Get("A"))
+
+	if !g.Adjacent(g.Get("B"), g.Get("A")) {
+		t.Fail()
+	}
+}
+
+func TestRemoveArc(t *testing.T) {
+	g := getTestGraph()
+	g.RemoveArc(g.Get("A"), g.Get("B"))
+
+	if g.Adjacent(g.Get("A"), g.Get("B")) {
+		t.Fail()
+	}
+}
+
 func BenchmarkAdd(b *testing.B) {
 	g := NewGraph()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		g.Add(NewNode(string(i)))
+	}
+}
+
+func BenchmarkAddArc(b *testing.B) {
+	g := NewGraph()
+
+	for i := 0; i < b.N; i++ {
+		g.Add(NewNode(string(i)))
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; i < b.N; i++ {
+			g.AddArc(g.Get(string(i)), g.Get(string(j)))
+		}
+	}
+}
+
+func BenchmarkRemoveArc(b *testing.B) {
+	g := NewGraph()
+
+	for i := 0; i < b.N; i++ {
+		g.Add(NewNode(string(i)))
+	}
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; i < b.N; i++ {
+			g.AddArc(g.Get(string(i)), g.Get(string(j)))
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; i < b.N; i++ {
+			g.RemoveArc(g.Get(string(i)), g.Get(string(j)))
+		}
 	}
 }
 
@@ -55,23 +130,4 @@ func BenchmarkRemove(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		g.Remove(g.Get(string(i)))
 	}
-}
-
-func getTestGraph() *Graph {
-	g := NewGraph()
-
-	a := NewNode("A")
-	b := NewNode("B")
-	c := NewNode("C")
-
-	g.Add(a)
-	g.Add(b)
-	g.Add(c)
-
-	g.AddArc(a, b)
-	g.AddArc(a, c)
-	g.AddArc(b, c)
-	g.AddArc(c, b)
-
-	return g
 }
