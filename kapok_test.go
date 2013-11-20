7dccbe6be5e251462316a9ba9809f226d2a46b4d
Lane Aasen <laneaasen@gmail.com>
2013-11-19 19:57:13 -0800

    deletes TestKapok, which will be replaced with more specific tests

--- kapok_test.go
+++ kapok_test.go
@@ -8,54 +8,7 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 
 	"github.com/aaasen/kapok/graph"
-	"github.com/aaasen/kapok/parse"
 )
-
-func TestKapok(t *testing.T) {
-	file, err := os.Open("/home/aasen/dev/data/enwiki-latest-pages-articles.xml")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pages := make(chan *parse.Page)
-	go parse.Parse(file, pages)
-
-	graph := graph.NewGraph()
-	numPages := 0
-	maxPages := 10
-
-	for {
-		select {
-		case page := <-pages:
-			log.Println(numPages)
-
-			origin := graph.SafeGet(page.Title)
-
-			for _, dest := range page.Links {
-				graph.AddArc(origin, graph.SafeGet(dest))
-			}
-
-			numPages++
-
-			if numPages >= maxPages {
-				file, err := os.Create("/home/aasen/dev/data/wiki-graph.gob")
-
-				if err != nil {
-					log.Fatal("error opening graph file: ", err)
-				}
-
-				err = graph.Export(file)
-
-				if err != nil {
-					log.Fatal("error exporting graph: ", err)
-				}
-
-				return
-			}
-		}
-	}
-}
 
 func TestGenerateGraph(t *testing.T) {
 	Convey("Generating a graph should work", t, func() {
