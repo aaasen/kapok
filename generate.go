192e4b2a0194a800d29295cca2c2489079c7ac9a
Lane Aasen <laneaasen@gmail.com>
2013-11-19 22:00:52 -0800

    adds GenerateAndStore which continuously generates and stores a graph to disk

--- generate.go
+++ generate.go
@@ -2,6 +2,7 @@ package kapok
 
 import (
 	"io"
+	"os"
 
 	"github.com/aaasen/kapok/graph"
 	"github.com/aaasen/kapok/parse"
@@ -40,4 +41,71 @@ func GenerateGraph(in io.Reader, maxPages int) *graph.Graph {
 			}
 		}
 	}
+}
+
+// GenerateAndStore generates a graph from the given reader and
+// continually exports it to disk.
+//
+// It will only consume maxPages pages, or unlimited if maxPages is -1
+//
+// saveInterval represents the number of pages between each export.
+func GenerateAndStore(in io.Reader, outPath string, maxPages, saveInterval int) error {
+	pages := make(chan *parse.Page)
+	go parse.Parse(in, pages)
+
+	graph := graph.NewGraph()
+	numPages := 0
+
+	for {
+		select {
+		case page, ok := <-pages:
+			numPages++
+
+			if !ok {
+				err := storeGraph(graph, outPath)
+
+				if err != nil {
+					return err
+				}
+			}
+
+			origin := graph.SafeGet(page.Title)
+
+			for _, dest := range page.Links {
+				graph.AddArc(origin, graph.SafeGet(dest))
+			}
+
+			if maxPages != -1 && numPages >= maxPages {
+				err := storeGraph(graph, outPath)
+
+				return err
+			}
+
+			if numPages%saveInterval == 0 {
+				err := storeGraph(graph, outPath)
+
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+}
+
+func storeGraph(g *graph.Graph, outPath string) error {
+	err := os.Remove(outPath)
+
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create(outPath)
+
+	if err != nil {
+		return err
+	}
+
+	g.Export(out)
+
+	return nil
 }
--- kapok_test.go
+++ kapok_test.go
@@ -27,3 +27,21 @@ func TestGenerateGraph(t *testing.T) {
 		So(len(g.Nodes), ShouldBeGreaterThanOrEqualTo, 100)
 	})
 }
+
+func TestGenerateAndStore(t *testing.T) {
+	Convey("Generating and storing should work", t, func() {
+		So(func() {
+			in, err := os.Open("/home/aasen/dev/data/enwiki-latest-pages-articles.xml")
+
+			if err != nil {
+				t.Fatal("error opening database file: ", err)
+			}
+
+			err = GenerateAndStore(in, "/home/aasen/dev/data/wiki-graph.gob", 10, 5)
+
+			if err != nil {
+				t.Fatal("error in GenerateAndStore: ", err)
+			}
+		}, ShouldNotPanic)
+	})
+}
