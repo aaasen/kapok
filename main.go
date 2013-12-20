193a467974d8d5b453baf49d63ccc0dea22441ea
Lane Aasen <laneaasen@gmail.com>
2013-12-20 12:08:46 -0800

    adds a little CLI for kapok with a generate command

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		return
+	}
+
+	switch os.Args[1] {
+	case "generate":
+		// "/home/aasen/dev/data/enwiki-latest-pages-articles.xml"
+		// "/home/aasen/downloads/articles.csv"
+		// "/home/aasen/downloads/rels.csv"
+
+		if len(os.Args) != 5 {
+			fmt.Println("generate requires exactly 3 arguments: xml path, nodes path, rels path")
+			return
+		}
+
+		GenerateByPath(os.Args[2], os.Args[3], os.Args[4])
+	}
+}
