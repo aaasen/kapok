830a3b77ff77e3e3b62446686374ad705a717114
Lane Aasen <laneaasen@gmail.com>
2013-12-20 11:38:11 -0800

    adds proof of concept for translating to csv and then importing. speeds are about 100 pages / second, which is so much better than Cyper. Complete csv translation should only take 12 hours on my laptop

--- main/main.go
+++ main/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
-	"github.com/aaasen/kapok"
+	"github.com/aaasen/kapok/generate"
+	"github.com/aaasen/kapok/parse"
 )
 
 func main() {
@@ -14,5 +16,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	kapok.GenerateNeoGraph(in, -1)
+	nodes, _ := os.Create("/home/aasen/downloads/articles.csv")
+	nodes.Write([]byte("title\tl:label\n"))
+
+	rels, _ := os.Create("/home/aasen/downloads/rels.csv")
+	rels.Write([]byte("start\tend\ttype\n"))
+
+	gen := generate.NewCSVGenerator()
+
+	pages := make(chan *parse.Page, 1024)
+	go parse.CategorizedParse(in, pages)
+
+	start := time.Now()
+	numPages := 0
+
+	for {
+		select {
+		case page, ok := <-pages:
+			if !ok {
+				log.Println("channel close, exiting")
+				return
+			}
+
+			gen.GeneratePage(page, nodes, rels)
+
+			if numPages%100 == 0 {
+				log.Printf("processed %v pages in %v", numPages, time.Since(start))
+			}
+
+			numPages++
+		}
+	}
 }
