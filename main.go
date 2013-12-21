7ba22f4bc3b2d8e0fca62b4da8198c59c448b0d8
Lane Aasen <laneaasen@gmail.com>
2013-12-21 15:23:53 -0800

    adds cpu and memory profiling options. looks like most of the cpu usage comes from parsing pages with regexes, and the largest memory consumers are xml encoding and id tracking

--- main.go
+++ main.go
@@ -2,28 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"runtime/pprof"
 
 	"github.com/aaasen/kapok/generate"
 )
+
+var cpuprofile = true
+var memprofile = true
 
 func main() {
 	if len(os.Args) <= 1 {
 		return
 	}
 
+	if cpuprofile {
+		f, err := os.Create("kapok_cpu.prof")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	switch os.Args[1] {
 	case "generate":
-		if len(os.Args) != 5 {
-			fmt.Println("generate requires exactly 3 arguments: xml path, nodes path, rels path")
+		if len(os.Args) < 5 {
+			log.Fatal("generate requires at least 3 arguments: xml path, nodes path, rels path")
 			return
 		}
 
-		err := generateByPath(os.Args[2], os.Args[3], os.Args[4], -1)
+		err := generateByPath(os.Args[2], os.Args[3], os.Args[4], 10000)
 
 		if err != nil {
 			fmt.Println(err)
 		}
+	default:
+		fmt.Println("command not found")
+	}
+
+	if memprofile {
+		f, err := os.Create("kapok_mem.prof")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pprof.WriteHeapProfile(f)
+		f.Close()
 	}
 }
 
